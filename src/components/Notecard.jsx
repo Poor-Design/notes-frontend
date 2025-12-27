60418b517cc039e888551434f3c90ff67a27a99f
revalen <prabesh123xyz@gmail.com>
2025-12-27 14:32:39 +0545

    fixed Note card css

--- src/components/Notecard.jsx
+++ src/components/Notecard.jsx
@@ -1,5 +1,5 @@
 import { Pencil, Trash2 } from 'lucide-react'
-import './NoteCard.css'
+import './Notecard.css'
 
 function NoteCard({ note, onEdit, onDelete }) {
   const formatDate = (dateString) => {
